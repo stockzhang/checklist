ddb0a93fc526c2499eeacc531619a003c4c0bb67
zhangwei <zhangwei@neoteched.com>
2017-03-09 17:06:48 +0800

    Tsocket

---
+++ checklist.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/3/9 16:50
+# @Author  : Stock Zhang
+# @Site    : 
+# @File    : checklist.py
+# @Software: PyCharm
+
+import sys
+import os
+
+if  __name__  == '__main__':
+    appName = sys.argv
+    print appName
